38d0829cc2469afb97fda46fe053d581a8f4c9c2
Ian <ianhathorn@gmail.com>
2022-04-14 21:17:01 -0500

    Fixed and working

--- src/SimpleRSA.java
+++ src/SimpleRSA.java
@@ -1,70 +1,104 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-public class SimpleRSA
-{
-   public static void main (String[] args)
-   {
-      try {
-   BigInteger e= new BigInteger("65537");
-   BigInteger d= new BigInteger("89489425009274444368228545921773093919669586065884257445497854456487674839629818390934941973262879616797970608917283679875499331574161113854088813275488110588247193077582527278437906504015680623423550067240042466665654232383502922215493623289472138866445818789127946123407807725702626644091036502372545139713");
-   BigInteger n= new BigInteger("145906768007583323230186939349070635292401872375357164399581871019873438799005358938369571402670149802121818086292467422828157022922076746906543401224889672472407926969987100581290103199317858753663710862357656510507883714297115637342788911463535102712032765166518411726859837988672111837205085526346618740053");
-   
-   //Read a string from the keyboard
-   BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-   System.out.println("Enter the message to encrypt");
-   String mstring = in.readLine();
-   
-   BigInteger m = convertStringToBigInteger(mstring);
-   BigInteger c = m.modPow(e, n); //Encryption
-   String cstring = convertBigIntegerToString(c);
-   System.out.println(cstring); //print the encrypted text
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class SimpleRSA{
+    public static void main(String[] args) {
+
+        try{
+
+            Random rand  = new SecureRandom();
+            BigInteger p = BigInteger.probablePrime(128, rand);
+            BigInteger q = BigInteger.probablePrime(128, rand);
+            BigInteger n = p.multiply(q);
+            BigInteger phi_n = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+            BigInteger e = new BigInteger("65537");
 
 
-   }
-   catch (Exception e) {
-         System.out.println("One or more of the assigned numbers were unable to compute, ending the program");
-   }
+            while (e.compareTo(BigInteger.ONE)<= 0 || e.compareTo(phi_n) >=0 )
+                if(e.gcd(phi_n).equals(BigInteger.ONE)) {
+                    break;
+                }
 
-  }
-  
+            BigInteger d = e.modInverse(phi_n);
+            BufferedReader in = new BufferedReader((new InputStreamReader(System.in)));
+            System.out.println("Enter the message to encrypt");
+            String mstring = in.readLine();
 
-//This is a method to convert a String to BigInteger by
-//packing each character into a BigInteger
-//Input: String
-//Output: BigInteger
-public static BigInteger convertStringToBigInteger(String s)
-{
-      BigInteger b = new BigInteger("0");
-      for (int i = 0; i < s.length(); i++)
-         {
-         Integer code = new Integer((int)s.charAt(i));
-         BigInteger c = new BigInteger(code.toString());
-         b = b.shiftLeft(8);
-         b = b.or(c);
-         }
-  return b;
-}
+            System.out.println("Original d = "+ d.toString());
+            BigInteger d1 = find_d(e,n);
+            System.out.println("d1 Found= "+d1.toString());
+
+            BigInteger m = convertStringToBigInteger(mstring);
+            long t1 = System.nanoTime();
+            BigInteger c = m.modPow(d, n);
+            long t2 = System.nanoTime();
+            System.out.println("Time Elapsed for d: "+ d.toString());
+            System.out.println(t2-t1);
+
+            d1 = new BigInteger("1100011100000011");
+            t1 = System.nanoTime();
+            m.modPow(d1, n);
+            t2 = System.nanoTime();
+            System.out.println("Time Elapsed for d1: "+ d1.toString());
+            System.out.println(t2-t1);
+
+            String cstring = convertBigIntegerToString(c);
+            System.out.println(cstring);
+
+            BigInteger g = c.modPow(e, n);
+            String mlstring = convertBigIntegerToString(g);
+            System.out.println(mlstring);
 
 
-//This is a method to convert a BigInteger to String
-//by converting each byte into a character and forming 
-//a string of characters.
-//Input: BigInteger
-//Output: String
+        } catch (Exception e) {
+            System.out.println("One or more of the assigned numbers were unable to compute, ending program");
+        }
 
-public static String  convertBigIntegerToString(BigInteger b)
-  { 
-  String s = new String();
-  while (b.compareTo(BigInteger.ZERO) == 1)
-         {
-         BigInteger c = new BigInteger("11111111", 2);
-         int cb = (b.and(c)).intValue();
-         Character cv= new Character((char)cb);
-         s = (cv.toString()).concat(s);
-         b = b.shiftRight(8);
-         }
-    return s;
-  }
 
+    }
+
+    public static BigInteger find_d(BigInteger e, BigInteger n){
+        boolean factorFound = false;
+        BigInteger p = n;
+        Random rand = new SecureRandom();
+        while (factorFound == false){
+            p = BigInteger.probablePrime(128, rand);
+            if (n.mod(p).equals(BigInteger.ZERO));
+                break;
+        }
+        BigInteger q = n.divide(p);
+        BigInteger phi_n = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        BigInteger d = e.modInverse(phi_n);
+        return d;
+    }
+
+    public static BigInteger convertStringToBigInteger(String s)
+    {
+        BigInteger b = new BigInteger("0");
+        for (int i = 0; i < s.length(); i++)
+        {
+            Integer code = (int) s.charAt(i);
+            BigInteger c = new BigInteger(code.toString());
+            b = b.shiftLeft(8);
+            b = b.or(c);
+        }
+        return b;
+    }
+
+    public static String  convertBigIntegerToString(BigInteger b)
+    {
+        String s = new String();
+        while (b.compareTo(BigInteger.ZERO) == 1)
+        {
+            BigInteger c = new BigInteger("11111111", 2);
+            int cb = (b.and(c)).intValue();
+            Character cv= (char) cb;
+            s = (cv.toString()).concat(s);
+            b = b.shiftRight(8);
+        }
+        return s;
+    }
 }
